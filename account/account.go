fdac1737869e12f151037f27c91f0f03891fb55a
Sina Sadeghi <sina.sadeghi83@gmail.com>
2024-07-14 22:09:21 +0330

    :sparkles: Support for RetrievePrice of Coin in account/userid and Add HandleConversion step 0

--- account/account.go
+++ account/account.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -12,7 +13,65 @@ import (
 
 type accountResponse struct {
 	model.Account
-	Uscent uint `json:"uscent"`
+	Uscent uint64 `json:"uscent"`
+}
+
+type ConversionForm struct {
+	UserID       int    `json:"user_id"`
+	SourceCoinID int    `json:"source_coin_id"`
+	DestCoinID   int    `json:"dest_coin_id"`
+	Amount       uint64 `json:"amount"`
+}
+
+func HandleConversion(w http.ResponseWriter, r *http.Request) {
+	db, err := db.NewConn()
+	if err != nil {
+		utils.EncodeResponse(w, r, http.StatusInternalServerError, "Internal Error")
+		return
+	}
+
+	reqData, err := utils.DecodeJson[ConversionForm](r)
+
+	if err != nil {
+		utils.EncodeResponse(w, r, http.StatusBadRequest, "Conversion form is not valid!")
+		return
+	}
+
+	if db.First(&model.User{}, reqData.UserID).RowsAffected == 0 {
+		utils.EncodeResponse(w, r, http.StatusNotFound, "User not found!")
+		return
+	}
+
+	conversion := model.Conversion{
+		SourceCoinID: reqData.SourceCoinID,
+		DestCoinID:   reqData.DestCoinID,
+		SourceAmount: reqData.Amount,
+		UserID:       reqData.UserID,
+		State:        0,
+	}
+
+	if err := conversion.CheckBalance(db); err != nil {
+		utils.EncodeResponse(w, r, http.StatusOK, "Insufficient Balance!")
+		return
+	}
+
+	conversion.LoadAssociates(db)
+	if err := conversion.CalculateExpected(); err != nil {
+		utils.EncodeResponse(w, r, http.StatusBadRequest, "Conversion is not valid!")
+		fmt.Println(err)
+		return
+	}
+
+	result := db.Create(&conversion)
+
+	if result.Error != nil {
+		utils.EncodeResponse(w, r, http.StatusBadRequest, "Conversion is not valid!")
+		fmt.Println(result.Error)
+		return
+	}
+	db.Save(&conversion)
+
+	utils.EncodeResponse(w, r, http.StatusOK, conversion)
 }
 
 func HandleGetAccount(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +80,7 @@ func HandleGetAccount(w http.ResponseWriter, r *http.Request) {
 
 	db, err := db.NewConn()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		utils.EncodeResponse(w, r, http.StatusInternalServerError, "Internal Error")
 		return
 	}
 
