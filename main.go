57ac838692bb91eee71b7ee9147a769ba85959fd
Sina Sadeghi <sina.sadeghi83@gmail.com>
2024-07-14 14:41:04 +0330

    :hammer: Setup db and Create dummy data to main

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"log"
 
+	"github.com/sinasadeghi83/SwapTask/db"
 	"github.com/sinasadeghi83/SwapTask/server"
 )
 
 func main() {
+	if _, err := db.SetupDB(); err != nil {
+		log.Fatal("Unable to setup db: ", err)
+		return
+	}
+
+	if err := db.CreateDummyData(); err != nil {
+		log.Fatal("Unable to create dummy data: ", err)
+		return
+	}
+
 	if err := server.NewServer().ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
