b34d9f8801d8da5cc026c94819e6aee8fbb4ef4a
Sina Sadeghi <sina.sadeghi83@gmail.com>
2024-07-14 14:43:47 +0330

    :hammer: Update user/HandleGetUser to support db

--- user/user.go
+++ user/user.go
@@ -1,40 +1,31 @@
 package user
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/sinasadeghi83/SwapTask/db"
+	"github.com/sinasadeghi83/SwapTask/model"
 )
 
-type User struct {
-	id    int
-	name  string
-	coins []map[Coin]int
-}
-
-type Coin struct {
-	id   int
-	name string
-}
-
-func (u User) String() string {
-	return fmt.Sprintf("id:%v, name:%s", u.id, u.name)
-}
-
 func HandleGetUser(w http.ResponseWriter, r *http.Request) {
-	users := map[int]User{
-		1: {id: 1, name: "sina"},
-		2: {id: 2, name: "ali"},
-		3: {id: 3, name: "hassan"},
-	}
-
 	vars := mux.Vars(r)
-	userID, err := strconv.Atoi(vars["id"])
+	userID, _ := strconv.Atoi(vars["id"])
+
+	db, err := db.NewConn()
 	if err != nil {
-		w.Write([]byte("Invalid user id!"))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte(users[userID].String()))
+	user := model.User{}
+	result := db.First(&user, userID)
+
+	if result.RowsAffected == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("User not found!"))
+		return
+	}
+
+	w.Write([]byte(user.String()))
 }
