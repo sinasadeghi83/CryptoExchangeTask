44a7cd5953e21a14ede836b6eb77e0ba6e526316
Sina Sadeghi <sina.sadeghi83@gmail.com>
2024-07-14 19:22:18 +0330

    :sparkles: Add balance to us cents in account/userid

--- account/account.go
+++ account/account.go
@@ -10,6 +10,11 @@ import (
 	"github.com/sinasadeghi83/SwapTask/utils"
 )
 
+type accountResponse struct {
+	model.Account
+	Uscent uint `json:"uscent"`
+}
+
 func HandleGetAccount(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	userID, _ := strconv.Atoi(vars["id"])
@@ -25,10 +30,29 @@ func HandleGetAccount(w http.ResponseWriter, r *http.Request) {
 	result := db.Where("user_id = ?", userID).Preload("Coin").Preload("User").Find(&accounts)
 
 	if result.Error != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal Error"))
+		utils.EncodeResponse(w, r, http.StatusInternalServerError, "Internal Error")
 		return
 	}
+	data, err := retrieveAccountWithCent(accounts)
 
-	utils.EncodeResponse(w, r, http.StatusOK, accounts)
+	if err != nil {
+		utils.EncodeResponse(w, r, http.StatusInternalServerError, "Could not retrieve the prices")
+		return
+	}
+	utils.EncodeResponse(w, r, http.StatusOK, data)
+}
+
+func retrieveAccountWithCent(accounts []model.Account) ([]accountResponse, error) {
+	data := make([]accountResponse, 0)
+	for _, account := range accounts {
+		uscent, err := account.CalculateUSCent()
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, accountResponse{
+			Account: account,
+			Uscent:  uscent,
+		})
+	}
+	return data, nil
 }
