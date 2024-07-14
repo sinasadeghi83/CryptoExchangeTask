205ea959bce8cf54b20bdefaa738380c4538f7a9
Sina Sadeghi <sina.sadeghi83@gmail.com>
2024-07-14 15:34:50 +0330

    :card_file_box: Add initial coins to database in db.go

--- db/db.go
+++ db/db.go
@@ -26,13 +26,34 @@ func CreateDummyData() error {
 		return err
 	}
 
-	users := []model.User{
+	users := generateFakeUsers()
+	if result := db.Create(users); result.Error != nil {
+		return result.Error
+	}
+
+	coins := generateInitialCoins()
+	if result := db.Create(coins); result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func generateFakeUsers() []model.User {
+	return []model.User{
 		{Name: "sina"},
 		{Name: "ali"},
 		{Name: "hassan"},
 	}
+}
 
-	result := db.Create(users)
-
-	return result.Error
+func generateInitialCoins() []model.Coin {
+	// Using smallest unit of each coin
+	return []model.Coin{
+		{Name: "USDT", Unit: "cent"},
+		{Name: "BTC", Unit: "satoshi"},
+		{Name: "ETH", Unit: "wei"},
+		{Name: "DOGE", Unit: "shibe"},
+		{Name: "XRP", Unit: "drop"},
+	}
 }
